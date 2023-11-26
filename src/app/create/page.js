7553afe3b17bf785a15cc8d927de837bfadfd4be
이솜이 <122191856+somyiLee@users.noreply.github.com>
2023-11-26 18:29:23 +0900

    Add data fetching logic from client component

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,3 +1,38 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Create() {
-  return <>Create!!</>;
+  const router = useRouter();
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const title = e.target.title.value;
+        const body = e.target.body.value;
+        const options = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ title, body }),
+        };
+
+        fetch("http://localhost:9999/topics", options)
+          .then((res) => res.json())
+          .then((data) => {
+            const lastid = data.id;
+            router.push(`/read/${lastid}`);
+          });
+      }}
+    >
+      <p>
+        <input type="text" name="title" placeholder="title" />
+      </p>
+      <p>
+        <textarea name="body" placeholder="body" />
+      </p>
+      <p>
+        <input type="submit" value="Create" />
+      </p>
+    </form>
+  );
 }
