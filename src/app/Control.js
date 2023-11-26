8982352169406a98b2efdc749cab35e87b2c5cd9
이솜이 <122191856+somyiLee@users.noreply.github.com>
2023-11-26 21:29:01 +0900

    Add logic to update and delete topic data

---
+++ src/app/Control.js
@@ -0,0 +1,39 @@
+"use client";
+
+import Link from "next/link";
+import { useParams, useRouter } from "next/navigation";
+
+export function Control() {
+  const router = useRouter();
+  const params = useParams();
+  const id = params.id;
+  return (
+    <ul>
+      <li>
+        <Link href="/create">Create</Link>
+      </li>
+      {id ? (
+        <>
+          <li>
+            <Link href={"/update/" + id}>Update</Link>
+          </li>
+          <li>
+            <input
+              type="button"
+              value="delete"
+              onClick={() => {
+                const options = { method: "DELETE" };
+                fetch("http://localhost:9999/topic/" + id, options)
+                  .then((res) => res.json())
+                  .then((result) => {
+                    router.push("/");
+                    router.refresh();
+                  });
+              }}
+            />
+          </li>
+        </>
+      ) : null}
+    </ul>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import "./globals.css";
+import { Control } from "./Control";
 
 export const metadata = {
   title: "Web tutorial",
@@ -29,17 +30,7 @@ export default async function RootLayout({ children }) {
           })}
         </ol>
         {children}
-        <ul>
-          <li>
-            <Link href="/create">Create</Link>
-          </li>
-          <li>
-            <Link href="/update/1">Update</Link>
-          </li>
-          <li>
-            <input type="button" value="delete" />
-          </li>
-        </ul>
+        <Control />
       </body>
     </html>
   );
